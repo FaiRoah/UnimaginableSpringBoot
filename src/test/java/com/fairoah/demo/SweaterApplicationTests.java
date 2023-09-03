dad1ae4daaf02efd6546a4b54802d339a2f6ca13
FaiRoah <namor2001@mail.ru>
2023-09-03 12:26:50 +0300

    Lesson 1

---
+++ src/test/java/com/fairoah/demo/SweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairoah.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
